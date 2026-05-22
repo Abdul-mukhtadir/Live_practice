96c3f3007641d8ac47b2e47845faa5177199459f
Abdul-mukhtadir <samshaik204@gmail.com>
2026-05-22 14:24:55 +0530

    use brevo api

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,27 +1,41 @@
 const nodemailer = require('nodemailer');
+const https = require('https');
 
 const sendResetEmail = async (toEmail, resetLink, userName) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-
-  await transporter.sendMail({
-    from: `"SecureApp Support" <${process.env.EMAIL_USER}>`,
-    to: toEmail,
+  const data = JSON.stringify({
+    sender: { name: 'SecureApp', email: 'ac1b43001@smtp-brevo.com' },
+    to: [{ email: toEmail }],
     subject: 'Password Reset Request - SecureApp',
-    html: `<p>Hello ${userName},</p>
-           <p>Click the link below to reset your password:</p>
-           <a href="${resetLink}">Reset My Password</a>
-           <p>This link expires in 60 minutes.</p>`
+    htmlContent: `<p>Hello ${userName},</p>
+                  <p>Click the link below to reset your password:</p>
+                  <a href="${resetLink}">Reset My Password</a>
+                  <p>This link expires in 60 minutes.</p>`
   });
 
-  console.log('Reset email sent to:', toEmail);
+  return new Promise((resolve, reject) => {
+    const options = {
+      hostname: 'api.brevo.com',
+      path: '/v3/smtp/email',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'api-key': process.env.BREVO_API_KEY,
+      }
+    };
+
+    const req = https.request(options, (res) => {
+      let body = '';
+      res.on('data', (chunk) => body += chunk);
+      res.on('end', () => {
+        console.log('Email sent:', body);
+        resolve(body);
+      });
+    });
+
+    req.on('error', reject);
+    req.write(data);
+    req.end();
+  });
 };
 
 module.exports = { sendResetEmail };
